c4e5a5601fcc76c8478197950d6ae8843da0d957
Jay Prajapati <jay.prajapati5717@gmail.com>
2025-08-16 01:21:22 +0530

    Chore: replace web hash history with web history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import store from '@/store'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
